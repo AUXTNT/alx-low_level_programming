824550ef6ebca74fa61eccc3dfa0e23fe69badf7
AUXTNT <auxtine4real@gmail.com>
2023-03-19 15:12:34 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,14 @@
  * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
- */int main(void)
+ */
+int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", i);
+		prints("%d", i);
 	}
 	putchar('\n');
 	return (0);
