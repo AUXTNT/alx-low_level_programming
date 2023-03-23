3d169dce02c0a330d2891e4335ae2d2736d8ab32
AUXTNT <auxtine4real@gmail.com>
2023-03-23 09:02:41 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int_putchar(char c);
-int_isupper(int c);
-int_isdigit(int c);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -13,4 +13,4 @@ void print_diagonal(int n);
 void print_square(int n);
 void print_triangle(int n);
 
-$endif
+#endif
