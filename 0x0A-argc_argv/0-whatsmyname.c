6586218584401185477a50bab06de26b7cd710e6
AUXTNT <auxtine4real@gmail.com>
2023-04-07 08:49:02 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n", *argc);
+
+	return (0);
+}
