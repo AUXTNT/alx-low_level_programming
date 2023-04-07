c2b921f4f10f3124fa986e2571e468c4f6a5253c
AUXTNT <auxtine4real@gmail.com>
2023-04-07 09:03:48 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
 
 /**
  * main - print the name of the program
- * @argc: the number of arguments
- * @argv: the array of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
