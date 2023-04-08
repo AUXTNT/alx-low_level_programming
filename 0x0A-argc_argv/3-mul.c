fd06894429d838d7b4cccb77acaa20a8c62b7b69
AUXTNT <auxtine4real@gmail.com>
2023-04-08 07:59:31 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,9 +7,7 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
@@ -19,17 +17,12 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-
 	while (s[len] != '\0')
-
 		len++;
-
 	while (i < len && f == 0)
-
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -43,7 +36,7 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	if (f == 0)
+	if (f == 0);
 		return (0);
 	return (n);
 }
