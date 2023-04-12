cc459e557a7df0abd2bb3477a61af24f812a8bd4
AUXTNT <auxtine4real@gmail.com>
2023-04-12 07:30:13 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int i, ci;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+		s1 = s2 = ci
 
 		i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
+	while (s1, s2, [i] != '\0')
+		i++, ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
