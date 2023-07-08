5d3aeb2964e2c81f0cb3b03a7cf162c969619993
AUXTNT <auxtine4real@gmail.com>
2023-07-08 13:17:21 -0700

    3-set_bit.c

--- 3-set_bit.c
+++ 3-set_bit.c
@@ -8,10 +8,23 @@
  * Return: success at 1, -1 at failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
+
+int main(void)
+{
+	unsigned long int number = 0;
+	unsigned int index = 3;
+
+	int result = set_bit(&number, index);
+	if (result == 1)
+
+	return 0;
+}
+
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 63)
-		return (-1);
+		return -1;
 
 	*n |= (1UL << index);
-	return (1);
+	return 1;
 }
